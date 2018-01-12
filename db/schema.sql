36a1125527886283f78eaeba4d8d417d7d89a37e
Kardo Jõeleht <kardoj@gmail.com>
2018-01-12 20:20:23 +0000

    Add Record's created_at and updated_at defaults

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ create table if not exists record(
 	id serial not null primary key,
 	heading varchar(200),
 	content text not null,
-	created_at timestamp without time zone,
-	updated_at timestamp without time zone,
+	created_at timestamp without time zone default 'now()',
+	updated_at timestamp without time zone default 'now()',
 	category_id int not null references category
 );
