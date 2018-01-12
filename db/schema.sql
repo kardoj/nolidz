c6e893a86946f8af69bfe639c1e9239db0f0f69f
Kardo Jõeleht <kardoj@gmail.com>
2018-01-12 19:52:10 +0000

    Yet another silly schema fix

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+create table if not exists category(
+        id serial not null primary key,
+        name varchar(50) not null
+);
+
 create table if not exists record(
 	id serial not null primary key,
 	heading varchar(200),
@@ -5,9 +10,4 @@ create table if not exists record(
 	created_at timestamp without time zone,
 	updated_at timestamp without time zone,
 	category_id int not null references category
-);
-
-create table if not exists category(
-	id serial not null primary key,
-	name varchar(50) not null
 );
