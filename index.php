cfd30f8018fe08762604ce4153c64fc020279137
Kardo Jõeleht <kardoj@gmail.com>
2018-01-28 17:32:28 +0000

    Accept record_id on index page

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 	require_once __DIR__ . '/services/Db.php';
 	require_once __DIR__ . '/services/RandomRecord.php';
 	require_once __DIR__ . '/renderers/RecordRenderer.php';
+	require_once __DIR__ . '/services/Records.php';
 	
 	$db = new Db();
-	$random_record = new RandomRecord($db);
+	$random_record_svc = new RandomRecord($db);
 ?>
 
 <!doctype html>
@@ -21,7 +22,18 @@
 		<?php require __DIR__ . '/searchbar.php'; ?>
 	
 		<div id="content">
-			<?php echo RecordRenderer::render($random_record->get()); ?>
+			<?php
+				$record = null;
+				if (isSet($_GET['record_id']) && !empty($_GET['record_id']))
+				{
+					$records_svc = new Records($db);
+					$record = $records_svc->find($_GET['record_id']);
+				}
+				
+				if (empty($record)) $record = $random_record_svc->get();
+
+				echo RecordRenderer::render(array($record));
+			?>
 		</div>
 	</div>
 	
--- services/RandomRecord.php
+++ services/RandomRecord.php
@@ -15,12 +15,12 @@ class RandomRecord
 				record.id as record_id,
 				record.heading as record_heading,
 				record.content as record_content,
-				category.id as category_id,
+				record.category_id,
 				category.name as category_name
 			from record 
 				inner join category on category.id = record.category_id
 			order by random()
 			limit 1
-		');
+		')[0];
 	}
 }
