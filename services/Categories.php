874d44791ddbaed14cfe61a3381a42c39ac4f9ab
Kardo Jõeleht <kardoj@gmail.com>
2018-01-28 17:31:35 +0000

    Add Records service

--- services/Categories.php
+++ services/Categories.php
@@ -18,23 +18,4 @@ class Categories
 			order by name asc
 		');
 	}
-
-	function find($name_or_id)
-	{
-		$db_call = $this->db->execute_params('
-			select id, name
-			from category
-			where id = $1 or name = $2
-			limit 1
-		', array(intval($name_or_id), $name_or_id));
-
-		if (count($db_call) > 0)
-		{
-			return $db_call[0];
-		}
-		else
-		{
-			return null;
-		}
-	}
 }
---
+++ services/Records.php
@@ -0,0 +1,38 @@
+<?php
+class Records
+{
+	private $db = null;
+	
+	function __construct($db)
+	{
+		$this->db = $db;
+	}
+	
+	function find($id)
+	{
+		$id = intval($id);
+		if (empty($id)) return null;
+
+		$db_call = $this->db->execute_params('
+			select
+				record.id as record_id,
+				record.heading as record_heading,
+				record.content as record_content,
+				record.category_id,
+				category.name as category_name
+			from record
+				inner join category on category.id = record.category_id
+			where record.id = $1
+			limit 1
+		', array($id));
+
+		if (count($db_call) > 0)
+		{
+			return $db_call[0];
+		}
+		else
+		{
+			return null;
+		}
+	}
+}
